e350591fb81433233d40637fb2e36699301f05f5
joannabec <mariela.benitezc9@gmail.com>
2022-05-01 17:57:49 -0500

    config for heroku backend

--- index.js
+++ index.js
@@ -34,6 +34,7 @@ app.use(express.json());
 app.use(cors());
 // app.use(morgan('tiny'));
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :post'))
+app.use(express.static('build'))
 
 app.get('/api/persons', (_, res) => {
   res.json(persons)
@@ -90,7 +91,7 @@ app.post('/api/persons', (req, res) => {
   }
 });
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 });

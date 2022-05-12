64f65accdcf493d9705dfcd03d85aa28507d4f4f
joannabec <mariela.benitezc9@gmail.com>
2022-05-12 18:25:05 -0500

    exercises3.15-3.18

--- index.js
+++ index.js
@@ -10,10 +10,10 @@ morgan.token('post', function (req) {
   return req.method === 'POST' && JSON.stringify(req.body);
 })
 
+app.use(express.static('build'));
 app.use(express.json());
 app.use(cors());
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :post'))
-app.use(express.static('build'));
 
 app.get('/api/persons', (_, res) => {
   Person.find({}).then(result => {
@@ -23,27 +23,32 @@ app.get('/api/persons', (_, res) => {
 
 app.get('/info', (_, res) => {
   const time = new Date().toString();
-  res.send(`
-    <p>Phonebook has info of ${persons.length} people</p>
+  Person.find({}).then(result => {
+    res.send(`
+    <p>Phonebook has info of ${result.length} people</p>
     <p>${time}</p>
-  `);
-;
+  `)
+  })
 });
 
-app.get('/api/persons/:id', (req, res) => {
-  const id = +req.params.id;
-  const item = persons.find(person => person.id === id);
+app.get('/api/persons/:id', (req, resp, next) => {
+  Person.findById(req.params.id)
+  .then(contact => {
+    if (contact) {
+      resp.json(contact)
+    } else {
+      resp.status(404).end()
+    }
+  })
+  .catch(error => next(error))
+})
 
-  if (item) res.json(item);
-  else res.status(404).end();
-  
-});
-
-app.delete('/api/persons/:id', (req, res) => {
-  const id = +req.params.id;
-  persons = persons.filter(person => person.id !== id);
-
-  res.status(204).end();
+app.delete('/api/persons/:id', (req, res, next) => {
+  Person.findByIdAndRemove(req.params.id)
+    .then(result => {
+      res.status(204).end()
+    })
+    .catch(error => next(error))
 });
 
 app.post('/api/persons', (req, res) => {
@@ -61,6 +66,36 @@ app.post('/api/persons', (req, res) => {
     res.json(savedPerson)
   });
 });
+
+app.put('/api/persons/:id', (req, res, next) => {
+  const body = req.body
+
+  const person = {
+    number: body.number,
+  }
+
+  Person.findByIdAndUpdate(req.params.id, person, { new: true })
+    .then(updatedContact => {
+      res.json(updatedContact)
+    })
+    .catch(error => next(error))
+})
+
+const unknownEndpoint = (req, res) => {
+  res.status(404).send({ error: 'unknown endpoint' })
+}
+app.use(unknownEndpoint)
+
+const errorHandler = (error, req, res, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return res.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {

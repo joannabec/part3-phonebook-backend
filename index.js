337485b9446047c53df1978ba8e60354f3c02d05
joannabec <mariela.benitezc9@gmail.com>
2022-05-15 15:52:42 -0500

    exercises3.19-3.21

--- index.js
+++ index.js
@@ -14,13 +14,13 @@ app.use(express.static('build'));
 app.use(express.json());
 app.use(cors());
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :post'))
-
+// GET ALL
 app.get('/api/persons', (_, res) => {
   Person.find({}).then(result => {
     res.json(result)
   })
 });
-
+// GET INFO
 app.get('/info', (_, res) => {
   const time = new Date().toString();
   Person.find({}).then(result => {
@@ -30,7 +30,7 @@ app.get('/info', (_, res) => {
   `)
   })
 });
-
+// GET BY ID
 app.get('/api/persons/:id', (req, resp, next) => {
   Person.findById(req.params.id)
   .then(contact => {
@@ -42,7 +42,7 @@ app.get('/api/persons/:id', (req, resp, next) => {
   })
   .catch(error => next(error))
 })
-
+// DELETE
 app.delete('/api/persons/:id', (req, res, next) => {
   Person.findByIdAndRemove(req.params.id)
     .then(result => {
@@ -50,23 +50,32 @@ app.delete('/api/persons/:id', (req, res, next) => {
     })
     .catch(error => next(error))
 });
-
-app.post('/api/persons', (req, res) => {
+// POST
+app.post('/api/persons', (req, res, next) => {
   const { name, number } = req.body;
-
+  
   if(!name || !number) {
     return res.status(400).json({ 
       error: 'name or number is missing' 
     })
   }
 
-  const person = new Person({ name, number });
+  Person.find({name}, function(_, contact) {
+    if (contact.length) {
+      return res.status(400).json({ 
+        error: 'name already exists' 
+      })
+    } else {
+      const person = new Person({ name, number });
 
-  person.save().then(savedPerson => {
-    res.json(savedPerson)
-  });
+      person.save().then(savedPerson => {
+        res.json(savedPerson)
+      }).catch((error) => next(error))
+    }
+  })
 });
 
+// PUT
 app.put('/api/persons/:id', (req, res, next) => {
   const body = req.body
 
@@ -80,18 +89,20 @@ app.put('/api/persons/:id', (req, res, next) => {
     })
     .catch(error => next(error))
 })
-
+// ENDPOINT
 const unknownEndpoint = (req, res) => {
   res.status(404).send({ error: 'unknown endpoint' })
 }
 app.use(unknownEndpoint)
-
+// ERROR
 const errorHandler = (error, req, res, next) => {
-  console.error(error.message)
-
+  console.log(error.message)
+  
   if (error.name === 'CastError') {
     return res.status(400).send({ error: 'malformatted id' })
-  } 
+  } else if (error.name === 'ValidationError') {
+    return res.status(400).json({ error: error.message })
+  }
 
   next(error)
 }
--- models/person.js
+++ models/person.js
@@ -11,8 +11,21 @@ mongoose.connect(url)
   })
   
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
+  name: {
+    type: String,
+    minLength: 3,
+    required: true
+  },
+  number: {
+    type: String,
+    required: true,
+    validate: {
+      validator: function(v) {
+        return /\d{2,3}-\d{6,}/.test(v);
+      },
+      message: props => `${props.value} is not a valid phone number!`
+    },
+  },
 })
 
 personSchema.set('toJSON', {
